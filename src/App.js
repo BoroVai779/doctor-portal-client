154d2717c497c74a60965a6c5126c780ad9ac925
sajib-rana <sajibrana444@gmail.com>
2022-11-14 02:19:09 +0600

    setup chair

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
     <div className="App">
      <Navbar></Navbar>
      <Routes>
-       <Route path='/home' element={<Home></Home>}></Route>
-       <Route path='/about' element={<About></About>}></Route>
-       <Route path='/home' element={<Home></Home>}></Route>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='About' element={<About></About>}></Route>
+       <Route path='Login' element={<Login></Login>}></Route>
+       
      </Routes>
     </div>
   );
