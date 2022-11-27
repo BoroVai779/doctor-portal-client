79397f7cf84624a06b675d4d19befd46b5fa6e97
sajib-rana <sajibrana444@gmail.com>
2022-11-28 00:44:37 +0600

    footer

--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const Reviews = ({review}) => {
-    return (
-      <div>
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-          <div class="card-body">
-            <p>{review.reviews}</p>
-          </div>
-          <div className='flex item-center p-5'>
-            <img src={review.img} alt="" />
-            <div className='pl-5'>
-                <h2 className='text-xl font-bold'>{review.name}</h2>
-                <h2>{review.state}</h2>
-            </div>
+const Reviews = ({ review }) => {
+  return (
+    <div>
+      <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+        <div className="card-body">
+          <p>{review.reviews}</p>
+        </div>
+        <div className="flex item-center p-5">
+          <img src={review.img} alt="" />
+          <div className="pl-5">
+            <h2 className="text-xl font-bold">{review.name}</h2>
+            <h2>{review.state}</h2>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Reviews;
